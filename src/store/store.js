abaede98d627005d85ace7592ebdd633f0df481e
Stephen Chiang <stephen.e.chiang@gmail.com>
2019-04-03 19:18:45 +0200

    env variable for middleware console output

--- src/store/store.js
+++ src/store/store.js
@@ -2,7 +2,9 @@ import { applyMiddleware, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './rootReducer';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = process.env.NODE_ENV === 'develop'
+	? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+	: null || compose;
 
 const store = createStore(
 	rootReducer,
